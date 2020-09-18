519ed17b24978f66e9b37f7d5f8e7656595f47ba
memematvaz <memematvaz@gmail.com>
2020-09-18 19:05:15 +0200

    data

---
+++ src/components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="card">
+        <h5 class="card_title"></h5>
+        <img src="" alt="">
+        <p class="card_description"></p>
+        <p class="card_price"></p>
+        <button class="card_button">Añadir</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Card'
+}
+</script>
+
+<style>
+
+</style>
