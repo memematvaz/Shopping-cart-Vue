6da878431dafee69526f3e48e619f7e25e7ea45c
memematvaz <memematvaz@gmail.com>
2020-09-21 23:40:17 +0200

    docs

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
+const path = require("path");
+
 module.exports = {
     chainWebpack: config => {
         config.module.rules.delete('eslint');
-    }
+    },
+    outputDir: path.resolve(__dirname, "./docs"),
+    assetsDir: "./public"
 }
